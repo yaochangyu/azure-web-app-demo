381320c0254ad577c27b4e286199716f55a0ad5f
yao <yao-chang.yu@outlook.com>
2026-01-31 07:57:18 +0800

    🔖 bump version 1.0.1

--- AspNetCoreApp/Models/VersionInfo.cs
+++ AspNetCoreApp/Models/VersionInfo.cs
@@ -2,7 +2,7 @@ namespace AspNetCoreApp.Models;
 
 public class VersionInfo
 {
-    public string Version { get; set; } = "1.0.0";
+    public string Version { get; set; } = "1.0.1";
     public string BuildDate { get; set; }
     public string Environment { get; set; } = "Production";
 }
