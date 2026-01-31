3c07ad77a7683faaed940b75880624291e944ade
yao <yao-chang.yu@outlook.com>
2026-01-31 09:07:45 +0800

    fix(VersionController): 更新版本號至 1.0.1

--- AspNetCoreApp/Controllers/VersionController.cs
+++ AspNetCoreApp/Controllers/VersionController.cs
@@ -14,7 +14,7 @@ public class VersionController : ControllerBase
         
         return new VersionInfo
         {
-            Version = "1.0.0",
+            Version = "1.0.1",
             BuildDate = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss"),
             Environment = env.EnvironmentName
         };
